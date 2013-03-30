4f7f588671afbe6c4f5136210912eb59ca311b07
Alexandru Csete <oz9aec@gmail.com>
2013-03-30 21:05:10 +0100

    strx: USe correct software name in license.

--- receiver/strx/receiver.h
+++ receiver/strx/receiver.h
@@ -2,12 +2,12 @@
 /*
  * Copyright (C) 2013 Alexandru Csete, OZ9AEC
  *
- * Gqrx is free software; you can redistribute it and/or modify
+ * Strx is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  *
- * Gqrx is distributed in the hope that it will be useful,
+ * Strx is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
