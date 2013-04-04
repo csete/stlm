08c3f12590c919b76e25bb73d0263692d1942fc5
Alexandru Csete <oz9aec@gmail.com>
2013-04-05 01:22:52 +0200

    strx-mon: Implemented read values.

--- receiver/strx-mon/strx-mon.cpp
+++ receiver/strx-mon/strx-mon.cpp
@@ -1,5 +1,5 @@
 #include <Ice/Ice.h>
-#include <gnuradio.h>
+#include "gnuradio.h"
 
 using namespace std;
 using namespace GNURadio;
@@ -11,6 +11,10 @@ int main(int argc, char* argv[])
 
     GNURadio::KnobIDList   empty_list;  // vector<string>
     GNURadio::KnobPropMap  knob_props;  // map<string, GNURadio::KnobProp>
+    GNURadio::KnobMap      knob_map;    // map<string, GNURadio::KnobPtr>
+    GNURadio::KnobPtr      knob;
+    GNURadio::KnobIPtr     knobi;
+    GNURadio::KnobDPtr     knobd;
 
     try
     {
@@ -22,12 +26,37 @@ int main(int argc, char* argv[])
         if (!ctrlport)
             throw "Invalid proxy";
 
-        // Get list of knobs
+        // Get list of knob properties
         knob_props = ctrlport->properties(empty_list);
-        cout << "Exported knobs: " << knob_props.size() << endl;
+        //for (KnobPropMap::iterator it = knob_props.begin(); it != knob_props.end(); ++it)
+        //    cout << "  " << it->first << endl;
 
-        for (KnobPropMap::iterator it = knob_props.begin(); it != knob_props.end(); ++it)
-            cout << "  " << it->first << endl;
+        // Get list of knobs
+        knob_map = ctrlport->get(empty_list);
+        cout << "Exported knobs: " << knob_map.size() << endl;
+
+        for (KnobMap::iterator it = knob_map.begin(); it != knob_map.end(); ++it)
+        {
+            cout << "  " << it->first << " = ";
+
+            knob = it->second;
+
+            switch (knob_props[it->first].type)
+            {
+            case KNOBINT:
+                knobi = static_cast<KnobIPtr>(knob);
+                cout << knobi->value << endl;
+                break;
+
+            case KNOBDOUBLE:
+                knobd = static_cast<KnobDPtr>(knob); // can also use: KnobPtr::dynamicCast(knob);
+                cout << knobd->value << endl;
+                break;
+
+            default:
+                cout << "(unsupported type)" << endl;
+            }
+        }
 
     }
     catch (const Ice::Exception& ex)
