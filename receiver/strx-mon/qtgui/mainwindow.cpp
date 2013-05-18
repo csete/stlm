520665698c82624e69f6a1844958dc4625c9c6f2
Alexandru Csete <oz9aec@gmail.com>
2013-05-18 12:54:35 +0200

    strx-mon: Use correct application name in file headers.

--- receiver/strx-mon/qtgui/mainwindow.cpp
+++ receiver/strx-mon/qtgui/mainwindow.cpp
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2013 Alexandru Csete, OZ9AEC
  *
- * Strx is free software; you can redistribute it and/or modify
+ * strx-mon is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  *
- * Strx is distributed in the hope that it will be useful,
+ * strx-mon is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
