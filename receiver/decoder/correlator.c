8ee32fa70ec4450ac06ecfdc2c70614ff5d48d67
Alexandru Csete <oz9aec@gmail.com>
2013-08-25 00:58:58 +0200

    decoder: Use correct aplication name.

--- receiver/decoder/correlator.c
+++ receiver/decoder/correlator.c
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Gqrx; see the file COPYING.  If not, write to
+ * along with strx; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
